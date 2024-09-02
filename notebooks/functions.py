13d7510d1d07c19d890128a2442f457f3804fa33
Tiago Pedro <tiagolpedro@gmail.com>
2024-09-02 15:28:10 +0100

    Addeded DB

---
+++ notebooks/functions.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import functions as func
+import yaml
+
+#function to import yaml file
+def import_yaml():
+    try:
+        with open("../config.yaml", "r") as file:
+            config = yaml.safe_load(file)
+    except:
+        print("The config.yaml file was not found in the main folder!")
+
+    return config
