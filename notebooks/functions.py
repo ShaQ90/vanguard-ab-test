8f277a81ddb67520fc1d80b3fdffdaece2385396
Tiago Pedro <tiagolpedro@gmail.com>
2024-09-03 20:16:35 +0100

    Addind testing

--- notebooks/functions.py
+++ notebooks/functions.py
@@ -11,3 +11,20 @@ def import_yaml():
         print("The config.yaml file was not found in the main folder!")
 
     return config
+
+
+
+def tukeys_test_outliers(data):
+    Q1 = data.quantile(0.25)
+    Q3 = data.quantile(0.75)
+    IQR = Q3 - Q1
+
+    # Define bounds for the outliers
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+
+    # Identify the outliers
+    outliers = data[(data < lower_bound) | (data > upper_bound)]
+
+    return outliers
+
